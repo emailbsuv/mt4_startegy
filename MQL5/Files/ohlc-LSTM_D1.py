79a723084d138e59a6d74dca87aad13f1ad739f8
Bogdan Suvorov <emailbsuv@gmail.com>
2023-08-23 05:58:08 +0300

    resultA

--- MQL5/Files/ohlc-LSTM_D1.py
+++ MQL5/Files/ohlc-LSTM_D1.py
@@ -9,6 +9,8 @@ def predict():
 	from datetime import datetime
 	# Устанавливаем уровень журналирования TensorFlow на ERROR
 	os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+	seq_length = 1399
+	opt = 0
 
 	# Создаем обучающий набор данных, разбивая данные на последовательные блоки (999 шагов входных данных, 1 шаг выходных данных)
 	def create_dataset(data, seq_length):
@@ -23,39 +25,78 @@ def predict():
 		data = data1[:, 3]
 		if "JPY" in valname:
 			data = np.array([value / 100 for value in data])
-		o = data[-1]
-		seq_length = 1399
+		o = data[-1]		
 		X_train, y_train = create_dataset(data, seq_length)
 		X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
-
-
-		# model = Sequential()
-		# model.reset_states()
-		# # Добавление слоев LSTM
-		# model.add(LSTM(units=64, return_sequences=True))
-		# model.add(LSTM(units=32))
-
-		# # Добавление полносвязных слоев
-		# model.add(Dense(units=16, activation='relu'))
-		# model.add(Dense(units=1))  # 1 выход для OHLC
-
-		# # Компиляция модели
-		# model.compile(optimizer='adam', loss='mean_squared_error')
-
-		# # Обучение модели
-		# model.fit(X_train, y_train, epochs=100, batch_size=64, verbose=0)
-		# model.save(valname+'.keras')
-
-		model = tf.keras.models.load_model(valname+'.keras')
+		if opt == 1:
+			model = Sequential()
+			model.reset_states()
+			model.add(LSTM(units=64, return_sequences=True))
+			model.add(LSTM(units=32))
+			model.add(Dense(units=16, activation='relu'))
+			model.add(Dense(units=1))  # 1 выход для OHLC
+			model.compile(optimizer='adam', loss='mean_squared_error')
+			model.fit(X_train, y_train, epochs=100, batch_size=64, verbose=0)
+			model.save(valname+'_Close.keras')
+		else:
+			model = tf.keras.models.load_model(valname+'_Close.keras')
 		last_5_candles = data[-seq_length:].reshape(1, seq_length, 1)
 		prv = model.predict(last_5_candles, verbose=0)
-
-		# Вывод результата прогнозирования
 		pov = 0.00001
 		c0 = prv[:,0]
 		c = c0[0]
 		c = int(round((c-o),5)/pov)
-		return [valname,c]
+		
+		data = data1[:, 2]
+		if "JPY" in valname:
+			data = np.array([value / 100 for value in data])
+		o = data[-1]		
+		X_train, y_train = create_dataset(data, seq_length)
+		X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+		if opt == 1:
+			model = Sequential()
+			model.reset_states()
+			model.add(LSTM(units=64, return_sequences=True))
+			model.add(LSTM(units=32))
+			model.add(Dense(units=16, activation='relu'))
+			model.add(Dense(units=1))  # 1 выход для OHLC
+			model.compile(optimizer='adam', loss='mean_squared_error')
+			model.fit(X_train, y_train, epochs=100, batch_size=64, verbose=0)
+			model.save(valname+'_Low.keras')
+		else:
+			model = tf.keras.models.load_model(valname+'_Low.keras')
+		last_5_candles = data[-seq_length:].reshape(1, seq_length, 1)
+		prv = model.predict(last_5_candles, verbose=0)
+		pov = 0.00001
+		l0 = prv[:,0]
+		l = l0[0]
+		l = int(round((l-o),5)/pov)		
+		
+		data = data1[:, 1]
+		if "JPY" in valname:
+			data = np.array([value / 100 for value in data])
+		o = data[-1]		
+		X_train, y_train = create_dataset(data, seq_length)
+		X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+		if opt == 1:
+			model = Sequential()
+			model.reset_states()
+			model.add(LSTM(units=64, return_sequences=True))
+			model.add(LSTM(units=32))
+			model.add(Dense(units=16, activation='relu'))
+			model.add(Dense(units=1))  # 1 выход для OHLC
+			model.compile(optimizer='adam', loss='mean_squared_error')
+			model.fit(X_train, y_train, epochs=100, batch_size=64, verbose=0)
+			model.save(valname+'_High.keras')
+		else:
+			model = tf.keras.models.load_model(valname+'_High.keras')
+		last_5_candles = data[-seq_length:].reshape(1, seq_length, 1)
+		prv = model.predict(last_5_candles, verbose=0)
+		pov = 0.00001
+		h0 = prv[:,0]
+		h = h0[0]
+		h = int(round((h-o),5)/pov)			
+		return [valname,c,h,l]
 
 
 	current_date = datetime.now()
@@ -70,28 +111,63 @@ def predict():
 		new_value = create_predict(np.genfromtxt(valname, delimiter=','), valname)
 		data1.append(new_value)
 	data1 = data1[1:]
-	def max_abs(row):
-	  # return max(abs(row[2][0]), abs(row[3][0]), abs(row[4][0]))
-	  return row[1]
-
-	data1.sort(key=max_abs, reverse=True) 
+	
 	result = []
 	for row in data1:
-	  result.append(row)
+		valname, c, h, l = row
+		result.append([
+			valname, 
+			int(c*0.4 + h*0.3 + l*0.3)
+		])	
+		
+	def Avg(row):
+	  # return max(abs(row[2][0]), abs(row[3][0]), abs(row[4][0]))
+	  return row[1]	
+	def Close(row):
+	  # return max(abs(row[2][0]), abs(row[3][0]), abs(row[4][0]))
+	  return row[1]
+	def Low(row):
+	  # return max(abs(row[2][0]), abs(row[3][0]), abs(row[4][0]))
+	  return row[3]
+	def High(row):
+	  # return max(abs(row[2][0]), abs(row[3][0]), abs(row[4][0]))
+	  return row[2]
+
+	result.sort(key=Avg, reverse=True) 
+	resultA = []
+	for row in result:
+	  resultA.append([row[0],row[1]])
+	  
+	data1.sort(key=Close, reverse=True) 
+	resultC = []
+	for row in data1:
+	  resultC.append([row[0],row[1]])
 	  # if (row[2][0] < 0 and row[4][0] < 0 and row[3][0] > 0) and min(abs(row[2][0]), abs(row[3][0]), abs(row[4][0]))>25:
 		# row[1][0] = "SELL 250 pips"
 		# result.append(row)
 	  # if (row[2][0] > 0 and row[4][0] > 0 and row[3][0] < 0) and min(abs(row[2][0]), abs(row[3][0]), abs(row[4][0]))>25:
 		# row[1][0] = "BUY 250 pips"
 		# result.append(row)
+		
+	data1.sort(key=Low, reverse=True) 
+	resultL = []
+	for row in data1:
+	  resultL.append([row[0],row[3]])
 
+	data1.sort(key=High, reverse=True) 
+	resultH = []
+	for row in data1:
+	  resultH.append([row[0],row[2]])
+	  
 	# print("Prediction W1 OHLC ",formatted_date)
 	file_name = "predict.log"
 	temp_file_name = "temp_" + file_name
 	with open(temp_file_name, "w") as file:
-		print("Predict D1 ohlC ",formatted_date, file=file)
-		for row in result:
-		  print(row, file=file)
+		print("Predict D1 oHLC ",formatted_date, file=file)
+		# for row in result:
+		  # print(row, file=file)
+		for row1, row2, row3, row4 in zip(resultC, resultH, resultL, resultA):
+			print(row1, " ", row2, " ", row3, " ", row4, file=file)
 		print(" ", file=file)
 		print(" ", file=file)
 	# Читаем содержимое оригинального файла и записываем его после нового содержимого
@@ -99,8 +175,8 @@ def predict():
 		temp_file.write(original_file.read())
 	os.remove(file_name)
 	os.rename(temp_file_name, file_name)
-	first_element = result[0][0]
-	last_element = result[-1][0]
+	first_element = resultA[0][0]
+	last_element = resultA[-1][0]
 	trimmed_first_element = first_element[:-7]
 	trimmed_last_element = last_element[:-7]
 	with open("predict.txt", "w") as file:
